1a614b3848a9b3dc36a9dabd8e6563bd86959034
Onur Akın <onur.akin@limonist.com>
2024-10-04 17:44:41 +0300

    status bug fixes

--- app/Http/Controllers/NotificationController.php
+++ app/Http/Controllers/NotificationController.php
@@ -11,7 +11,7 @@ use Illuminate\Http\Request;
 use App\Models\NotificationSettings;
 use App\Models\UserNotificationSettings;
 use Carbon\Carbon;
-use Illuminate\Support\Facades\DB;
+
 
 class NotificationController extends Controller
 {
@@ -20,16 +20,15 @@ class NotificationController extends Controller
      */
     public function NotificationSettings(Request $request)
     {
-    
+        try{
             $user = $request->user();
             $userId = $user->id;
             $languageType = $request->header('Accept-Language');
 
-            $notification_settings = DB::table('notification_settings')
-            ->join('user_notification_settings', 'notification_settings.id', '=', 'user_notification_settings.notification_setting_id')
-            ->where('user_notification_settings.user_id', $userId)
-            ->select('notification_settings.id', 'notification_settings.title', 'user_notification_settings.status', 'notification_settings.is_active') // is_active alanını da seçiyoruz
-            ->get();
+            $notification_settings = NotificationSettings::whereHas('status', function ($query) use ($userId) {
+                $query->where('user_id', $userId);
+            })->get();
+            
        
     
             // Sadece id, title ve is_active (status) alanlarını içeren yeni bir yapı oluştur
@@ -49,7 +48,17 @@ class NotificationController extends Controller
                     'items' => $filtered_data
                 ]
             ]);
-       
+        }
+        catch (\Exception $e) {
+            // Dil bilgisine göre hata mesajını ayarla
+            $errorMessage = ($languageType === 'tr') ? 'Sunucu hatası oluştu' : 'Server error occurred';
+    
+            // Hata durumunda JSON yanıtı döndür
+            return response()->json([
+                'status' => false,
+                'message' => $errorMessage, // Hata mesajı
+            ], 500); // 500 sunucu hatası kodu
+        }
 
     }
 
