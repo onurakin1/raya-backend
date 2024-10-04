278064a6bb845dd04387d1ee54ebb2f2e3491fab
Onur Akın <onur.akin@limonist.com>
2024-10-04 10:44:34 +0300

    Code bug fix

--- app/Http/Controllers/ChannelController.php
+++ app/Http/Controllers/ChannelController.php
@@ -180,7 +180,7 @@ class ChannelController extends Controller
         try{
             $languageType = $request->header('Accept-Language');
             $roomCode = $request->query('code');
-            $room = Rooms::where('id', $roomCode)->first();
+            $room = Rooms::where('room_code', $roomCode)->first();
     
             // 4 haneli kullanıcı adı oluşturma (rastgele sayı)
             $username = str_pad(rand(0, 9999), 4, '0', STR_PAD_LEFT);
