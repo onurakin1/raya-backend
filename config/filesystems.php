c93a934cd734d3a7827da8282a930e1b3de968cd
Onur Akın <onur.akin@limonist.com>
2024-10-04 16:35:36 +0300

    file path changed

--- config/filesystems.php
+++ config/filesystems.php
@@ -39,12 +39,11 @@ return [
 
         'public' => [
             'driver' => 'local',
-            'root' => storage_path('app/public'),
-            'url' => env('APP_URL').'/storage',
+            'root' => public_path('uploads'),
+            'url' => env('APP_URL').'/uploads',
             'visibility' => 'public',
-            'throw' => false,
+    
         ],
-
         's3' => [
             'driver' => 's3',
             'key' => env('AWS_ACCESS_KEY_ID'),
