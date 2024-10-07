9843abab0d99ca88c7404d6217752b65f8d013ff
Onur Akın <onur.akin@limonist.com>
2024-10-07 14:07:13 +0300

    login get

--- routes/api.php
+++ routes/api.php
@@ -56,6 +56,7 @@ Route::post('/send-message', [CallCenterController::class, 'SendMessage'])->midd
 Route::post('/register', [AuthController::class, 'register']);
 Route::post('/disable-endpoint', [AuthController::class, 'disableEndpoint']);
 Route::post('/upload', [FileUploadController::class, 'upload']);
-Route::post('/login', [AuthController::class, 'login']);
+Route::get('login', ['as' => 'login', 'uses' => 'AuthController@login']);
+
 Route::post('/refresh-token', [CallCenterController::class, 'refreshToken'])->middleware('auth:sanctum');
 Route::post('/logout', [AuthController::class, 'logout'])->middleware('auth:sanctum');
